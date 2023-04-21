f3b2ce47e0445ee85f1658f2e1e8de1afca1bd12
lapaset <liisa.r.lahti@gmail.com>
2023-04-21 13:08:31 +0200

    Add another note

---
+++ src/notes/AnotherNote.svelte
@@ -0,0 +1,1 @@
+<h1>Another note</h1>
--- src/routes/note/[slug]/+page.ts
+++ src/routes/note/[slug]/+page.ts
@@ -2,14 +2,18 @@ import type { ComponentType } from "svelte";
 import type { NoteParams, NoteType } from "../../../types/note.type";
 import NotFound from "../../../notes/NotFound.svelte";
 import GettingStarted from "../../../notes/GettingStarted.svelte";
+import AnotherNote from "../../../notes/AnotherNote.svelte";
 
 const getNote = (slug: string): ComponentType => {
   switch (slug.toLowerCase()) {
     case 'getting-started': {
-      return GettingStarted
+      return GettingStarted;
+    }
+    case 'another-note': {
+      return AnotherNote;
     }
     default: {
-      return NotFound
+      return NotFound;
     }
   }
 }
